c6aa3b6c39c6ff9c2ac6b2a5c37597468fc1f9ef
esrmnt <nitish.er@gmail.com>
2024-01-13 16:58:50 +0530

    Initial Commit

---
+++ src/Extractor/Program.cs
@@ -0,0 +1,12 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.ResourceManager.ApiManagement;
+using Azure.ResourceManager.ApiManagement.Models;
+
+class Program
+{
+}
+
